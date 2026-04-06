f45d6032e78a8410db277cc4a7373f93c783602e
simple9920 <simple9920@gmail.com>
2026-04-06 21:37:12 +0900

    Phase2 自分のデータ一覧取得と delete 修正

--- routers/items.py
+++ routers/items.py
@@ -17,6 +17,12 @@ def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db),
                 current_user: User = Depends(get_current_user)):
     return crud.create_item(db, item, current_user.id)
 
+def get_my_items(
+        db: Session = Depends(get_db),
+        current_user: User = Depends(get_current_user)
+):
+    return crud.get_items_by_user(db, current_user.id)
+
 @router.get("/{item_id}", response_model=ItemResponse)
 def get_item(
     item_id: int,
@@ -48,9 +54,9 @@ def update_item(item_id: int,
 def delete_item(
     item_id: int, 
     db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user.id)
+    current_user: User = Depends(get_current_user)
 ):
-    item = crud.delete_item(db, item_id)
+    item = crud.delete_item(db, item_id, current_user.id)
 
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
